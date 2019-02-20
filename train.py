e6c2e70c4d114e5f1c81bbaf4736a49744715464
ZQPei <ZQPei@github.com>
2019-02-20 22:48:24 +0800

    faster_rcnn

--- train.py
+++ train.py
@@ -38,6 +38,15 @@ im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
 im_data, im_scale_ratio = preprocess(im)
 im_info = np.array([*im_data.shape[:2], im_scale_ratio])
 net.eval()
-res = net.detect(im_data, im_info)
+dets, scores, classes = net.detect(im_data, im_info)
+
+for i, det in enumerate(dets):
+    det = tuple(int(x) for x in det)
+    cv2.rectangle(im2show, det[0:2], det[2:4], (255, 205, 51), 2)
+    cv2.putText(im2show, '%s: %.3f' % (classes[i], scores[i]), (det[0], det[1] + 15), cv2.FONT_HERSHEY_PLAIN,
+                1.0, (0, 0, 255), thickness=1)
+cv2.imshow('demo', im2show)
+cv2.waitKey(0)
+
 
 from IPython import embed; embed()
