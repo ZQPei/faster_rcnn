191420dff483ab3b839d2a34529bc81eb1d400d8
ZQPei <ZQPei@github.com>
2019-02-20 19:41:42 +0800

    faster_rcnn

--- faster_rcnn/network/nms/nms_cpu_np.py
+++ faster_rcnn/network/nms/nms_cpu_np.py
@@ -40,17 +40,14 @@ def NMS2(dets, threshold):
     """
     assert dets.ndim == 2 and dets.shape[1] == 5, "input error of dets"
 
-    x1 = dets[:,0]
-    y1 = dets[:,1]
-    x2 = dets[:,2]
-    y2 = dets[:,3]
-    score = dets[:,4]
+    x1 = dets[:, 0]
+    y1 = dets[:, 1]
+    x2 = dets[:, 2]
+    y2 = dets[:, 3]
+    scores = dets[:, 4]
 
-    # 1 compute areas
-    areas = (x2-x1+1) * (y2-y1+1)
-
-    # 2 sort score
-    order = score.argsort()[::-1]
+    areas = (x2 - x1 + 1) * (y2 - y1 + 1)
+    order = scores.argsort()[::-1]
 
     # 3 del bbox of those IoU greater than threshold
     keep = []
@@ -60,16 +57,16 @@ def NMS2(dets, threshold):
         # compute IoU
         xx1 = np.maximum(x1[i], x1[order[1:]])
         yy1 = np.maximum(y1[i], y1[order[1:]])
-        xx2 = np.maximum(x2[i], x2[order[1:]])
-        yy2 = np.maximum(y2[i], y2[order[1:]])
+        xx2 = np.minimum(x2[i], x2[order[1:]])
+        yy2 = np.minimum(y2[i], y2[order[1:]])
 
         w = np.maximum(0.0, xx2 - xx1 + 1)
         h = np.maximum(0.0, yy2 - yy1 + 1)
-        inter_area = w * h
-        IoU = inter_area/(areas[i]+areas[order[1:]]-inter_area)
+        inter = w * h
+        ovr = inter / (areas[i] + areas[order[1:]] - inter)
 
-        inds = np.where(IoU<threshold)[0]
-        order = order[inds+1]
+        inds = np.where(ovr <= thresh)[0]
+        order = order[inds + 1]
 
     mask = np.array(keep, dtype=np.int64)
     return mask
