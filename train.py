69b9b918d9f00d618abafa6801fe1799b6e1e7a8
ZQPei <ZQPei@github.com>
2019-02-20 19:36:10 +0800

    faster_rcnn

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 
 from faster_rcnn.datasets.pascal_voc import Pascal_VOC
 from faster_rcnn.roi_data_layer.roidb import prepare_roidb
@@ -8,6 +9,7 @@ from faster_rcnn.network.faster_rcnn import FasterRCNN
 
 from faster_rcnn.config import cfg
 np.random.seed(cfg.SEED)
+torch.manual_seed(cfg.SEED)
 
 # data_layer
 if cfg.DATASET.NAME == 'Pascal_VOC':
