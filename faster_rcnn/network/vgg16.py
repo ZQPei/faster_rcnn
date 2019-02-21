80e1ead309585065403b7f2ae89287c6010f1ba6
ZQPei <ZQPei@github.com>
2019-02-22 00:05:43 +0800

    faster_rcnn

--- faster_rcnn/network/vgg16.py
+++ faster_rcnn/network/vgg16.py
@@ -69,11 +69,11 @@ def load_pretrained_npy(vgg16, fname):
     vgg16_dict = vgg16.state_dict()
     import ipdb; ipdb.set_trace()
     for name, val in vgg16_dict.items():
-        if name.find('bn.') >= 0:
+        if name.find(b'bn.') >= 0:
             continue
         i, j = int(name[4]), int(name[6]) + 1
-        ptype = 'weights' if name[-1] == 't' else 'biases'
-        key = 'conv%d_%d'%(i, j)
+        ptype = b'weights' if name[-1] == 't' else b'biases'
+        key = b'conv%d_%d'%(i, j)
         param = torch.from_numpy(params[key][ptype])
 
         if ptype == b'weights':
