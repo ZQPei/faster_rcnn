064b0ccb8fca9f7361f2da3adfaa363ab3a27fa2
ZQPei <ZQPei@github.com>
2019-02-21 19:56:19 +0800

    faster_rcnn

--- faster_rcnn/network/nms/__init__.py
+++ faster_rcnn/network/nms/__init__.py
@@ -1,9 +1,11 @@
 from .nms_cpu_np import NMS as nms_cpu
 from .nms_gpu_torch import NMS as nms_gpu
+from .gpu_nms import gpu_nms
 
 from ...config import cfg
 
-nms = nms_gpu if cfg.USE_CUDA else nms_cpu
+# nms = nms_gpu if cfg.USE_CUDA else nms_cpu
+nms = gpu_nms
 
 
 
