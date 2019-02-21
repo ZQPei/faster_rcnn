eb35d7c976aff088601d3ef640b1628ceebf85a9
ZQPei <ZQPei@github.com>
2019-02-21 19:42:03 +0800

    faster_rcnn

--- faster_rcnn/network/nms/nms_test.py
+++ faster_rcnn/network/nms/nms_test.py
@@ -32,7 +32,7 @@ dets = torch.tensor([[3, 3, 6, 6, 0.7, 0.8, 0.2],
         [4, 4, 6, 6, 0.8, 0.3, 0.7], 
         [8.5, 8.5, 7, 7, 0.5, 0.4, 0.6]]).cuda()
 
-mask = torch.zeros((dets.size()[0], dets.size()[1])).cuda()
+mask = torch.zeros_like(dets[:,:5]).cuda()
 nms.nms(dets[:,:5], mask, 0.5)
 
 from IPython import embed; embed()
