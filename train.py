daa6bae70ae682d4127c04993951224ecd73464f
ZQPei <ZQPei@github.com>
2019-02-23 21:39:22 +0800

    faster_rcnn

--- train.py
+++ train.py
@@ -61,7 +61,7 @@ lr /= 0.01
 # Optimizer
 params = list(net.parameters())[8:]
 optimizer = torch.optim.SGD(params, lr, momentum=momentum, weight_decay=weight_decay, dampening=dampening)
-lr_schedular = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones, gamma=gamma, last_epoch=start_step-1)
+lr_schedular = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones, gamma=gamma, last_epoch=start_step)
 
 # Start training
 t = Timer()
