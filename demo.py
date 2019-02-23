16cdc3c33ab701f1083384a14ae25b6752b43614
ZQPei <ZQPei@github.com>
2019-02-23 20:23:03 +0800

    faster_rcnn

--- demo.py
+++ demo.py
@@ -27,25 +27,26 @@ t = Timer()
 image_files = [os.path.join(demo_image_dir,x) 
                 for x in os.listdir(demo_image_dir) if x[-4:] == '.jpg']
 
-for image_name in image_files:
-    print(image_name)
-    im = PIL.Image.open(image_name)
-    im = np.array(im)
-    im_data, im_scale = preprocess(im)
-    im_info = np.array([*im_data.shape[:2], im_scale])
-    
-    t.tic()
-    bboxes, scores, cls_inds = net.detect(im_data, im_info, demo_thresh)
-    runtime = t.toc(average=False)
+with torch.no_grad():
+    for image_name in image_files:
+        print(image_name)
+        im = PIL.Image.open(image_name)
+        im = np.array(im)
+        im_data, im_scale = preprocess(im)
+        im_info = np.array([*im_data.shape[:2], im_scale])
+        
+        t.tic()
+        bboxes, scores, cls_inds = net.detect(im_data, im_info, demo_thresh)
+        runtime = t.toc(average=False)
 
-    import ipdb; ipdb.set_trace()
+        import ipdb; ipdb.set_trace()
 
-    print('total spend: {}s'.format(runtime))
-    cls_str = [cfg.DATASET.CLASSES[x] for x in cls_inds]
-    im2show = draw_bbox(im, bboxes, scores, cls_str, im_color_mode='RGB')
+        print('total spend: {}s'.format(runtime))
+        cls_str = [cfg.DATASET.CLASSES[x] for x in cls_inds]
+        im2show = draw_bbox(im, bboxes, scores, cls_str, im_color_mode='RGB')
 
-    cv2.imshow(image_name, im2show)
-    cv2.waitKey(0)
+        cv2.imshow(image_name, im2show)
+        cv2.waitKey(0)
 
 
 
