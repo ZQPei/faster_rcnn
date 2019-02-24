4e1c34fc910e4d92f5ce71e9bcd1dd702d9bdd96
ZQPei <ZQPei@github.com>
2019-02-24 11:16:10 +0800

    faster_rcnn

--- demo.py
+++ demo.py
@@ -18,6 +18,7 @@ demo_image_dir = cfg.DEMO_IMAGE_DIR
 demo_model_file = cfg.DEMO_MODEL_FILE
 demo_thresh = cfg.DEMO_THRESH
 
+net = FasterRCNN(21)
 net = load_net(net, demo_model_file)
 net.eval()
 net.cuda()
